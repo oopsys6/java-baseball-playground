ce03b983fc2d7009016843fed63dc885d4dc4731
oopsys6 <operasystem6@naver.com>
2021-06-01 18:15:06 +0900

    refactor(string_calculator): 문자열 나눠주는 로직 분리

--- src/main/java/string_calculator/Input.java
+++ src/main/java/string_calculator/Input.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 
 public class Input {
 
-    public static String[] getStringValues(Scanner scanner) {
+    public static String getStringValue(Scanner scanner) {
         String value = scanner.nextLine();
+        return value;
+    }
+
+    public static String[] splitStringValues(String value) {
         String[] values = value.split(" ");
         return values;
     }
