a4fb3d1bc82cf04eb81ec198e3a839cdb4441bc8
oopsys6 <operasystem6@naver.com>
2021-07-17 11:05:42 +0900

    feat: plus, minus, multiply, divide 연산해주는 Operator enum 클래스 구현

---
+++ src/main/java/string_calculator/Operator.java
@@ -0,0 +1,45 @@
+package string_calculator;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum Operator {
+    PLUS("+", (first, second) -> first + second),
+    MINUS("-", (first, second) -> first - second),
+    MULTIPLY("*", (first, second) -> first * second),
+    DIVIDE("/", (first, second) -> {
+        if (second == 0) {
+            throw new IllegalArgumentException();
+        }
+        return first / second;
+    });
+
+    private static final Map<String, Operator> operatorMap =
+            Collections.unmodifiableMap(Stream.of(values()).collect(Collectors.toMap(operator -> operator.getSymbol(), operator -> operator)));
+    private String symbol;
+    private BiFunction<Integer, Integer, Integer> operation;
+
+    Operator(String symbol, BiFunction<Integer, Integer, Integer> operation) {
+        this.symbol = symbol;
+        this.operation = operation;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int operate(int first, int second) {
+        return operation.apply(first, second);
+    }
+
+    public static Operator findOperator(String symbol) {
+        Operator operator = operatorMap.get(symbol);
+        if (operator == null) {
+            throw new IllegalArgumentException();
+        }
+        return operator;
+    }
+}
